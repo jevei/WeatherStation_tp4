4fdbb5966344fe5a6c0ff447d3a3ad4d357d3c50
Jérémy Veillette <1830942@cshawi.ca>
2020-11-13 10:15:02 -0500

    test

---
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -0,0 +1,55 @@
+﻿using OpenWeatherAPI;
+using System;
+using Xunit;
+
+namespace OpenWeatherAPITests
+{
+    public class OpenWeatherProcessorTests : IDisposable
+    {
+        OpenWeatherProcessor _sut = new OpenWeatherProcessor();
+        [Fact]
+        public void GetOneCallAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+            // Arrange
+
+            // Act     
+            
+            // Assert
+            Assert.IsType<ArgumentException>(_sut.GetOneCallAsync().Exception.InnerException);
+        }
+        [Fact]
+        public void GetCurrentWeatherAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+            // Arrange
+
+            // Act     
+
+            // Assert
+            Assert.IsType<ArgumentException>(_sut.GetCurrentWeatherAsync().Exception.InnerException);
+        }
+        [Fact]
+        public void GetOneCallAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+            // Arrange
+
+            // Act       
+
+            // Assert
+            Assert.IsType<ArgumentException>(_sut.GetOneCallAsync().Exception.InnerException);
+        }
+        [Fact]
+        public void GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+            // Arrange
+
+            // Act       
+
+            // Assert
+            Assert.IsType<ArgumentException>(_sut.GetCurrentWeatherAsync().Exception.InnerException);
+        }
+        public void Dispose()
+        {
+
+        }
+    }
+}
